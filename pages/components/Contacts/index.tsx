1b3bd790235c5a0e759bffb4a400e3139335172a
emoises <eduardo.moises@gmail.com>
2021-09-19 03:39:06 -0300

    correcao de links do contato e responsividade da barra de botoes

--- pages/components/Contacts/index.tsx
+++ pages/components/Contacts/index.tsx
@@ -13,13 +13,13 @@ const Contacts: React.FC = () => {
         <FontAwesomeIcon className={styles.images} icon={faWhatsapp} color="#333" size={"2x"} />
         <span className={styles.span}>Whatsapp</span>
       </a>
-      <a className={styles.links} href="https://github.com/emoises"
+      <a className={styles.links} href="https://www.linkedin.com/in/eduardomoises/"
       target="_blank" rel="noreferrer"
       >
         <FontAwesomeIcon className={styles.images} icon={faLinkedinIn} color="#333" size={"2x"} />
         <span className={styles.span}>Linkedin</span>
       </a>
-      <a className={styles.links} href="https://www.linkedin.com/in/eduardomoises/"
+      <a className={styles.links} href="https://github.com/emoises"
       target="_blank" rel="noreferrer"
       >
         <FontAwesomeIcon className={styles.images} icon={faGithubAlt} color="#333" size={"2x"} />
