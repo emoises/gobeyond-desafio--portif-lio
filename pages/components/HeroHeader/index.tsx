3b15b4adcd2d81850d681b531a51a2bdf50f8c80
emoises <eduardo.moises@gmail.com>
2021-09-19 01:27:24 -0300

    resolvendo deploy na vercel

--- pages/components/HeroHeader/index.tsx
+++ pages/components/HeroHeader/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image'
-import styles from '../../../styles/HeroHeader.module.css'
-import heroImg from '../../../../public/images/profile.jpg'
+import styles from '../../../styles/heroHeader.module.css'
+import heroImg from '../../../public/images/profile.jpg'
 const HeroHeader: React.FC = () => {
   
   return (
